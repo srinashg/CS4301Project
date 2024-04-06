5bb6631c0f557c61911552536483bdb412a88752
Jeremy Nguyen <89870941+jeremu2907@users.noreply.github.com>
2024-04-06 18:34:07 -0500

    Log in, signup, proxying request to backend (#6)
    * Log in, signup, proxying request to backend
    
    * Soft loading

---
+++ client/utils.js
@@ -0,0 +1,16 @@
+import Toastify from 'toastify-js'
+
+export const proxy = 'http://localhost:8000'
+
+export const showNotif = (text) => {
+    Toastify({
+        text: text,
+        duration: 3000,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        style: {
+            background: "linear-gradient(to right, rgb(44, 130, 187),  rgb(243, 150, 255))",
+        }
+    }).showToast();
+}
